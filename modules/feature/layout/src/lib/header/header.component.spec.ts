0e32b6d2c4f0e99efe2e31974b480ceaf3263ffa
Andre Palhares <apalhares@ecisolutions.com>
2025-04-06 12:08:38 +0100

    refactor(header): adding dynamic required title to header

--- modules/feature/layout/src/lib/header/header.component.spec.ts
+++ modules/feature/layout/src/lib/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 
+const mockTitle = 'Mock Title';
+
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
@@ -12,6 +14,7 @@ describe('HeaderComponent', () => {
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    component.title = mockTitle;
     fixture.detectChanges();
   });
 
@@ -21,7 +24,6 @@ describe('HeaderComponent', () => {
 
   it('should cointain a title', () => {
     const header: HTMLHeadElement = fixture.nativeElement.querySelector('header');
-
-    expect(header.textContent).toBe('Ecommerce');
+    expect(header.textContent).toBe(mockTitle);
   });
 });
