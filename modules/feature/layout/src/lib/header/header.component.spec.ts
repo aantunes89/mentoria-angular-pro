e2f48d282ed51e32637f1f0383161f99b38869ba
apalhares <apalhares@ecisolutions.com>
2025-05-24 17:37:34 +0100

    feat(layout): add href to title

--- modules/feature/layout/src/lib/header/header.component.spec.ts
+++ modules/feature/layout/src/lib/header/header.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 const mockTitle = 'Mock Title';
 
@@ -10,6 +11,7 @@ describe('HeaderComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
@@ -30,5 +32,12 @@ describe('HeaderComponent', () => {
 
     fixture.detectChanges();
     expect(header.textContent).toBe('another title');
+  });
+
+  it('should redirect to "/" when logo is is clicked', () => {
+    const anchor: HTMLAnchorElement = fixture.nativeElement.querySelector('a');
+    // captura no DOM o elemento <a> e verifica se o href é igual a "/"
+    // comparamos o href pq o browser não entende o routerLink apenas o href
+    expect(anchor.getAttribute('href')).toBe('/');
   });
 });
