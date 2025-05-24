4d5fb875ce9e7e740160b9c33bb06a823b9df9ed
apalhares <apalhares@ecisolutions.com>
2025-05-24 18:04:25 +0100

    feat(home): add recomended products

--- modules/feature/home/src/lib/home/home.component.ts
+++ modules/feature/home/src/lib/home/home.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatCardModule } from '@angular/material/card';
+
+import { MOCK_PRODUCTS } from '@ecommerce/product-data-access';
 @Component({
   selector: 'lib-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss',
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  public products = MOCK_PRODUCTS;
+}
