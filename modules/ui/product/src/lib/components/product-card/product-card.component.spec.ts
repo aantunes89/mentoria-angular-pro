75f47e8ebd957e2e0ec19eb3d6b58a56435adcdd
Andre <andrepalhares1989@gmail.com>
2025-09-01 08:36:09 +0100

    feat(product-ui): moved product-card to its own folder and added tests

--- modules/ui/product/src/lib/components/product-card/product-card.component.spec.ts
+++ modules/ui/product/src/lib/components/product-card/product-card.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ProductCardComponent } from './product-card.component';
+import { MOCK_PRODUCTS } from '@ecommerce/product-data-access';
 
 describe('ProductCardComponent', () => {
   let component: ProductCardComponent;
@@ -12,10 +13,18 @@ describe('ProductCardComponent', () => {
 
     fixture = TestBed.createComponent(ProductCardComponent);
     component = fixture.componentInstance;
+    component.product = MOCK_PRODUCTS[0];
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render product info correctly', () => {
+    const card: HTMLElement = fixture.nativeElement.querySelector('mat-card');
+    expect(card).toBeTruthy();
+    expect(card.textContent).toContain(component.product.name);
+    expect(card.textContent).toContain(component.product.price);
   });
 });
---
+++ modules/ui/product/src/lib/components/product-card/product-card.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+
+import { Product } from '@ecommerce/product-data-access';
+
+@Component({
+  selector: 'lib-product-card',
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ProductCardComponent {
+  @Input({ required: true }) product!: Product;
+}
