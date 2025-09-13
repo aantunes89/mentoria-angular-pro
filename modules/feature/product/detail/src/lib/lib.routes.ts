6bbb9b3ff5dd1df7d75e47c49e5deaa8c3363de2
Andre Antunes <andre-antunes-sova@Mac.Home>
2025-09-13 18:58:56 +0100

    adding lib

---
+++ modules/feature/product/detail/src/lib/lib.routes.ts
@@ -0,0 +1,4 @@
+import { Route } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+
+export const productDetailRoutes: Route[] = [{ path: '', component: ProductDetailComponent }];
