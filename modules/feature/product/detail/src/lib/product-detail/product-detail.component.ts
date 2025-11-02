8c4655efdedc8ea5f737f4b3c5ecd5d66826d5e4
andre <andre.antunes@sovaassessment.com>
2025-11-02 20:25:57 +0000

    added cart component

--- modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
+++ modules/feature/product/detail/src/lib/product-detail/product-detail.component.ts
@@ -2,22 +2,25 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Observable, switchMap } from 'rxjs';
 
-import { Product, ProductSearchService } from '@ecommerce/product-data-access';
+import { MatButtonModule } from '@angular/material/button';
+
+import { CartService, Product, ProductSearchService } from '@ecommerce/product-data-access';
 import { ProductCardComponent } from '@ecommerce/product-ui';
 
 import { ROUTE_ID, ROUTE_ID_PROVIDERS } from './get-params';
 import { QuantityDescriptionPipe } from '../pipes';
 
 @Component({
-  selector: 'lib-product-detail',
+  selector: 'e-commerce-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss',
   standalone: true,
-  imports: [CommonModule, ProductCardComponent, QuantityDescriptionPipe],
+  imports: [CommonModule, ProductCardComponent, QuantityDescriptionPipe, MatButtonModule],
   providers: [ROUTE_ID_PROVIDERS],
 })
 export class ProductDetailComponent {
   productSearchService = inject(ProductSearchService);
+  cartService = inject(CartService);
 
   readonly product$: Observable<Product> = inject(ROUTE_ID).pipe(
     switchMap((id) => this.productSearchService.getById(id))
--- modules/ui/product/src/lib/components/product-card/product-card.component.ts
+++ modules/ui/product/src/lib/components/product-card/product-card.component.ts
@@ -5,7 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { Product } from '@ecommerce/product-data-access';
 
 @Component({
-  selector: 'lib-product-card',
+  selector: 'e-commerce-product-card',
   standalone: true,
   imports: [CommonModule, MatCardModule],
   templateUrl: './product-card.component.html',
