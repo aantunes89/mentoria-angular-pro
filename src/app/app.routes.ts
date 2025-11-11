2d6a4ed316c07d695bc276e6e95fa744e541762f
andre <andre.antunes@sovaassessment.com>
2025-11-11 09:13:57 +0000

    feat(auth-form): added lib

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
-import { Route } from "@angular/router";
+import { Route } from '@angular/router';
 
 export const APP_ROUTES: Route[] = [
   {
-    path: "",
-    pathMatch: "full",
-    redirectTo: "home",
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
   },
   {
-    path: "home",
-    loadChildren: () => import("@ecommerce/home").then((c) => c.HOME_ROUTES),
+    path: 'home',
+    loadChildren: () => import('@ecommerce/home').then((c) => c.HOME_ROUTES),
   },
   {
-    path: "product",
-    loadChildren: () => import("@ecommerce/product-detail").then((c) => c.PRODUCT_DETAIL_ROUTES),
+    path: 'product',
+    loadChildren: () => import('@ecommerce/product-detail').then((c) => c.PRODUCT_DETAIL_ROUTES),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('@ecommerce/auth-form').then((c) => c.AUTH_FORM_ROUTES),
   },
 ];
